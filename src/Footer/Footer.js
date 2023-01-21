5387499a0118c9741b309a3a531e3c698865658c
jay522 <jayrajverma7777@gmail.com>
2023-01-21 20:13:24 +0530

    first

---
+++ src/Footer/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import './Footer.css'
+export const Footer = () => {
+  return (
+    <div className='position-fixed  footer text-center bg-primary text-white'>&copy; rights belongs to me.</div>
+  )
+}
